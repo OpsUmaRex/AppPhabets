2d9d3fc62e35581bdd5cae16a502e00cf305518b
Adam Cholewinski <adam.cholewinski@loop.colum.edu>
2017-05-09 10:24:49 -0500

    URL script

---
+++ Assets/Scripts/OpenURL.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenURL : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Application.OpenURL("https://docs.google.com/document/d/1QhCrTE8C6Q0_DrQUugwmQ3X3eVw7na2QMBFp8LTXPj0/edit");
+    }
+}
+
+
